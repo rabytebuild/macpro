c78d5ef80c5eb5ddfe687776c132eca43f00349d
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2023-11-17 22:51:28 +0100

    TCPX

--- configure.sh
+++ configure.sh
@@ -30,4 +30,4 @@ brew install --cask ngrok
 
 # Configure ngrok and start it
 ngrok authtoken 2Hd7yeF4INCKbg2aP9rGMLnDqBX_5K7WhATjW8eUxS6UoHSRa
-ngrok tcp 5900 &
+ngrok tcp readily-touching-teal.ngrok-free.app 5900 &
