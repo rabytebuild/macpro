a72885118a87e1eaea5fb7df3bc1e00705c921e0
Rabyte Build <115883793+rabytebuild@users.noreply.github.com>
2023-11-17 22:59:40 +0100

    GTcpX

--- configure.sh
+++ configure.sh
@@ -30,4 +30,4 @@ brew install --cask ngrok
 
 # Configure ngrok and start it
 ngrok authtoken 2Hd7yeF4INCKbg2aP9rGMLnDqBX_5K7WhATjW8eUxS6UoHSRa
-ngrok tcp -subdomain=readily-touching-teal 5900 &
+ngrok tcp 5900 &
